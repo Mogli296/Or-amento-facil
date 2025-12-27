20e14eedbed74b6cc6789640f9f89b7aac7e88bc
Mogli296 <contatomoglitattoo@gmail.com>
2025-12-27 01:13:01 -0300

    Update forms.py

--- cadastros/forms.py
+++ cadastros/forms.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.core.validators import RegexValidator
-from .models import Client
+from .models import Client, Produto, Shipping
 
 # Validador simples para telefones brasileiros
 phone_validator = RegexValidator(
@@ -9,6 +9,9 @@ phone_validator = RegexValidator(
     message="Número de telefone inválido. Use o formato (XX) XXXXX-XXXX."
 )
 
+# =========================
+# Formulário de Cliente
+# =========================
 class ClientForm(forms.ModelForm):
     phone = forms.CharField(
         validators=[phone_validator],
@@ -23,4 +26,20 @@ class ClientForm(forms.ModelForm):
 
     class Meta:
         model = Client
+        fields = '__all__'
+
+# =========================
+# Formulário de Produto
+# =========================
+class ProdutoForm(forms.ModelForm):
+    class Meta:
+        model = Produto
+        fields = '__all__'
+
+# =========================
+# Formulário de Frete
+# =========================
+class ShippingForm(forms.ModelForm):
+    class Meta:
+        model = Shipping
         fields = '__all__'
