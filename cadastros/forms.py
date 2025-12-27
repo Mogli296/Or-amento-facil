6133621fe783b4737c85c94d1e17761767fd70f0
Mogli296 <contatomoglitattoo@gmail.com>
2025-12-27 00:23:40 -0300

    Update forms.py

--- cadastros/forms.py
+++ cadastros/forms.py
@@ -1,84 +1,26 @@
-from django.forms import ModelForm
-from django.forms.models import inlineformset_factory
+# -*- coding: utf-8 -*-
 from django import forms
-from .models import *
-# from .models import Produto
-# from .models import Client
-# from .models import Shipping
-# from .models import ProductShip 
+from django.core.validators import RegexValidator
+from .models import Client
 
-from localflavor.br.forms import BRStateSelect
-from localflavor.br.forms import BRPhoneNumberField
-from localflavor.br.forms import BRZipCodeField
-from localflavor.br.forms import BRCPFField
-from django_localflavor_br.forms import BRCNPJField
-from django_localflavor_br.forms import BRPhoneNumberField
-from django_countries.widgets import CountrySelectWidget
-
-
-class ProdutoForm(forms.ModelForm):
-	# def save(self, user, commit=True):
-	# 	produto = forms.ModelForm.save(commit=False)
-	# 	produto.user = user
-	# 	if commit:
-	# 		produto.save()
-	# 	return produto
-	
-
-	class Meta:
-		model = Produto
-		exclude = ('user',)
-
-
+# Validador simples para telefones brasileiros
+phone_validator = RegexValidator(
+    regex=r'^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$',
+    message="Número de telefone inválido. Use o formato (XX) XXXXX-XXXX."
+)
 
 class ClientForm(forms.ModelForm):
-	
-	class Meta:
-		model = Client
-		form = Client
-		widgets = { 'state': BRStateSelect(),'country': CountrySelectWidget() }
-		fields = ['name', 'cpf', 'phone', 'cep', 'cnpj', 'state']
-	
-	
-	cpf = BRCPFField()
-	cep = BRZipCodeField(required=True)
-	cnpj = BRCNPJField(label='CNPJ', required=False)
-	phone = BRPhoneNumberField(label='Telefone')
+    phone = forms.CharField(
+        validators=[phone_validator],
+        required=False,
+        label="Telefone"
+    )
+    celphone = forms.CharField(
+        validators=[phone_validator],
+        required=False,
+        label="Celular"
+    )
 
-
-class ShippingForm(forms.ModelForm):
-
-	class Meta:
-		model = Shipping
-		exclude = ('user',)
-		form = Shipping
-# class ProductShipForm(forms.ModelForm):
-
-# 	class Meta:
-# 		model = ProductShip
-
-# 	ProductShipFormset = inlineformset_factory(Shipping,Produto)
-class ServiceForm(forms.ModelForm):
-	
-	class Meta:
-		model = Service
-		exclude = ('user',)
-		# form = Service
-
-class TermsForm(forms.ModelForm):
-	
-	class Meta:
-		model = Term
-		exclude = ('user',)
-
-class GuaranteeForm(forms.ModelForm):
-	
-	class Meta:
-		model = Guarantee
-		exclude = ('user',)
-
-class PaymentForm(forms.ModelForm):
-	
-	class Meta:
-		model = Payment
-		exclude = ('user',)
+    class Meta:
+        model = Client
+        fields = '__all__'
