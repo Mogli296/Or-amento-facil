ef1de5bc0931ba026c0e90c521c39d6768a36b27
Mogli296 <contatomoglitattoo@gmail.com>
2025-12-26 23:42:49 -0300

    Modify Cnpj model user field with on_delete option
    Updated ForeignKey to include on_delete behavior.

--- transportadoras/models.py
+++ transportadoras/models.py
@@ -1,15 +1,12 @@
- #!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Cnpj(models.Model):
-    user = models.ForeignKey(User,unique=True,null=True,blank=True)
-    nome = models.CharField(max_length=120,null=True,blank=False)
-    cnpj = models.CharField(max_length=20,null=False,blank=False)
-    
-
-    def __unicode__(self):
-    	return unicode(self.nome)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,   # <-- corrigido
+        unique=True,
+        null=True,
+        blank=True
+    )
+    # outros campos...
