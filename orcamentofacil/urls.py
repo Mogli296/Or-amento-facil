b9021c49fee8cc056922419a27d561baf65686e4
Mogli296 <contatomoglitattoo@gmail.com>
2025-12-27 00:45:44 -0300

    Refactor URL patterns to use path() instead of url()

--- orcamentofacil/urls.py
+++ orcamentofacil/urls.py
@@ -1,29 +1,28 @@
-from django.conf.urls import *
+# -*- coding: utf-8 -*-
 from django.contrib import admin
-admin.autodiscover()
+from django.urls import path, include
+from orcamentos.views import base
+from cadastros import views as cadastros_views
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'orcamentofacil.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'orcamentos.views.base', name='base'),
-    url(r'^cadastros/$', 'cadastros.views.cadastros', name='cadastros'),
-    # url(r'^edit/$', 'cadastros.views.edit_produtos', name='edit_produtos'),
-    url(r'^members/(?P<username>\w+)/$', 'cadastros.views.add_produtos', name='add_produtos'),
-    #url(r'^cadastros/produto/(?P<id>\d+)/$', 'cadastros.views.delete_produtos', name='delete_produtos'),
-    url(r'^cadastros/produto/(?P<id>\d+)/$', 'cadastros.views.deletar', name='deletar'),
-    url(r'^cadastros/client/(?P<id>\d+)/$', 'cadastros.views.delete_clients', name='delete_clients'),
-    url(r'^cadastros/shipping/(?P<id>\d+)/$', 'cadastros.views.delete_shippings', name='delete_shippings'),
-    url(r'^single_produto/(?P<id>.*)/$', 'cadastros.views.edit_produtos', name='edit_produtos'),
-    url(r'^edit_clients/(?P<id>.*)/$', 'cadastros.views.edit_clients', name='edit_clients'),
-    url(r'^add_clients/$', 'cadastros.views.add_clients', name='add_clients'),
-    url(r'^add_shippings/$', 'cadastros.views.add_shippings', name='add_shippings'),
-    url(r'^edit_shippings/(?P<id>.*)/$', 'cadastros.views.edit_shippings', name='edit_shippings'),
-    # url(r'^add_productshippings/$', 'cadastros.views.add_productshippings', name='add_productshippings'),
-    # url(r'^members/(?P<username>\w+)/$', 'cadastros.views.produto_table', name='produto_table'),
-    # url(r'^$', 'orcamentos.views.navbar', name='navbar'),
-    (r'^accounts/', include('registration.urls')),
-    # url(r'^people/$', 'cadastros.views.people', name='people'),
+urlpatterns = [
+    # Admin
+    path('admin/', admin.site.urls),
 
-)
+    # Página inicial
+    path('', base, name='base'),
+
+    # Cadastros
+    path('cadastros/', cadastros_views.cadastros, name='cadastros'),
+    path('members/<str:username>/', cadastros_views.add_produtos, name='add_produtos'),
+    path('cadastros/produto/<int:id>/', cadastros_views.deletar, name='deletar'),
+    path('cadastros/client/<int:id>/', cadastros_views.delete_clients, name='delete_clients'),
+    path('cadastros/shipping/<int:id>/', cadastros_views.delete_shippings, name='delete_shippings'),
+    path('single_produto/<int:id>/', cadastros_views.edit_produtos, name='edit_produtos'),
+    path('edit_clients/<int:id>/', cadastros_views.edit_clients, name='edit_clients'),
+    path('add_clients/', cadastros_views.add_clients, name='add_clients'),
+    path('add_shippings/', cadastros_views.add_shippings, name='add_shippings'),
+    path('edit_shippings/<int:id>/', cadastros_views.edit_shippings, name='edit_shippings'),
+
+    # Accounts (django-registration)
+    path('accounts/', include('registration.backends.default.urls')),
+]
