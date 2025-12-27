01e29d314305dba0d3a9bf5e88fd101d98478ea1
Mogli296 <contatomoglitattoo@gmail.com>
2025-12-26 23:49:59 -0300

    Fix imports and update CnpjForm fields
    Updated import statements and adjusted fields in CnpjForm.

--- transportadoras/forms.py
+++ transportadoras/forms.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 from django import forms
-from django_localflavor_br.forms import BRCNPJField
-from models import Cnpj
-
+from localflavor.br.forms import BRCNPJField   # corrigido
+from .models import Cnpj                       # corrigido
 
 class CnpjForm(forms.ModelForm):
-	
-	cnpj = BRCNPJField(label='CNPJ', required=False)
-    
-class Meta:
-	model = Cnpj
+    cnpj = BRCNPJField(label='CNPJ', required=False)
 
+    class Meta:
+        model = Cnpj
+        fields = ['user', 'cnpj']   # ajuste conforme os campos que você quer no form
