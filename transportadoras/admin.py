173b66ae37814104d2ee2734db255ed674bd5da3
Mogli296 <contatomoglitattoo@gmail.com>
2025-12-26 23:47:40 -0300

    Fix CnpjAdmin to use __str__ method
    Updated CnpjAdmin to use __str__ instead of __unicode__.

--- transportadoras/admin.py
+++ transportadoras/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from models import Cnpj
-from forms import CnpjForm
+from .models import Cnpj          # <-- corrigido
+from .forms import CnpjForm       # <-- corrigido
 
 class CnpjAdmin(admin.ModelAdmin):
-	list_display = ['__unicode__', 'cnpj']
-	form = CnpjForm
-
+    list_display = ['__str__', 'cnpj']  # usa __str__ em vez de __unicode__
+    form = CnpjForm
 
 admin.site.register(Cnpj, CnpjAdmin)
